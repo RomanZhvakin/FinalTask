f63c9c47370c27958da039a74db2a575d0a078b9
Roman <roman_zhvakin@mail.ru>
2022-08-23 18:14:19 +0700

    Контрольная работа

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,46 +1,39 @@
-﻿/* **Задача**: Написать программу, которая из имеющегося массива
-строк формирует массив из строк, длина которых меньше либо 
-равна 3 символа. Первоначальный массив можно ввести с клавиатуры,
-либо задать на старте выполнения алгоритма. При решении 
-не рекомендуется пользоваться коллекциями, 
+﻿/* Задача: Написать программу, которая из имеющегося массива строк 
+формирует массив из строк, длина которых меньше либо 
+равна 3 символа. 
+Первоначальный массив можно ввести с клавиатуры,
+либо задать на старте выполнения алгоритма. 
+При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами.
-**Примеры**:
-[hello,2,world,:-)] -> ["2", ":-)"]
-[1234,1567,-2,computer science] -> ["-2"]
-[Russia,Denmark,Kazan] -> [] */
+Примеры:
+["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+["1234", "1567", "-2", "computer science"] -> ["-2"]
+["Russia", "Denmark", "Kazan"] -> [] */
 
-System.Console.WriteLine("Введите количество элементов массива: \t");
+string[] array = new string[4] { "floppa", "flip", "flop", ":-)" }; //задан массив из 4-х эл-в
+string[] SortedArray = new string[array.Length]; //объявляется отсортированный массив
 
-string[] CreateAndShowArr(int size)
+void ArraySorting(string[] array, string[] SortedArray) //метод сортировки массива
 {
-    string[] str = new string[size];
-    for (int i = 0; i < str.Length; i++)
-    {
-        System.Console.WriteLine($"Введите элемент №{i + 1}");
-        str[i] = Console.ReadLine()!;
-    }
-    Console.ForegroundColor = ConsoleColor.Red;
-    System.Console.Write("\nИзначальный массив: [");
-    foreach (var item in str)
-    {
-        Console.Write(item + ", ");
-    }
-    System.Console.Write("\b \b]");
-    Console.ForegroundColor = ConsoleColor.White;
-    return str;
-}
-string[] str = CreateAndShowArr(int.Parse(Console.ReadLine()!));
+    int count = 0; //счетчик для прохода по элементам массива
 
-string[] SelectThreeDigitValues (string[] str)
-{
-    string[] selected = new string[str.Length];
-    for (int i = 0; i < str.Length; i++)
+    for (int i = 0; i < array.Length; i++) //поиск элементов меньше 3-х символов
     {
-        selected[i] = str[i].Length <= 3 ? str[i] : null;
+        if (array[i].Length <= 3) 
+        {
+            SortedArray[count] = array[i];
+            count++;
+        }
     }
-    selected = selected.Where(x => x != null).ToArray();
-    return selected;
 }
-Console.ForegroundColor = ConsoleColor.Green;
-System.Console.WriteLine($"\nОтсортированный массив: [{string.Join(", ", SelectThreeDigitValues(str))}]");
-Console.ForegroundColor = ConsoleColor.White; 
+
+void PrintArray(string[] array) //вывод 
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+ArraySorting(array, SortedArray);
+PrintArray(SortedArray); 
